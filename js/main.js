431d07956a2d62c36f29410b319059a8f569b4e2
Vandana Pai <vandanapai01@gmail.com>
2015-07-25 15:54:26 -0700

    for mobile and tablet, added margins to outer container

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#project-tile-boost").click(function() {
-    	$("#main-content-container").load("work/boost2.html");
+    	$("#main-content-container").load("work/boost.html");
     });
+
 });
