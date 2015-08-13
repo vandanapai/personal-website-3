901f81df46c52d96899b1a732ef77731b5add6e2
Vandana Pai <vandana@vertigo.com>
2015-08-13 13:34:21 -0700

    added play glyph over static images to indicate playability

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
 	$(function(){
 		$("a[rel='get']").click(function(e){
 		e.preventDefault();
@@ -11,6 +12,8 @@ $(document).ready(function() {
 			// change color of VP logo		
 			pageColor = $(this).attr('data-color');
 			$('#background-fill, #p-fill').attr('class', pageColor);
+
+
 
 		//to get the ajax content and display in div with id 'content'
 		$.ajax({url:pageurl+'?rel=get',success: function(data){
@@ -33,6 +36,56 @@ $(document).ready(function() {
 			}});
 		});
 	});
+
+	(function($) {
+
+	  // Get the .gif images from the "data-alt".
+		var getGif = function() {
+			var gif = [];
+			$('img').each(function() {
+				var data = $(this).data('alt');
+				gif.push(data);
+			});
+			return gif;
+		}
+
+		var gif = getGif();
+
+		// Preload all the gif images.
+		var image = [];
+
+		$.each(gif, function(index) {
+			image[index]     = new Image();
+			image[index].src = gif[index];
+		});
+
+		// Change the image to .gif when clicked and vice versa.
+		$(document).on('click', "figure[name='paused-gif']", function() {
+
+			var $this   = $(this),
+					$index  = $this.index(),
+					
+					$img    = $this.children('img'),
+					$imgSrc = $img.attr('src'),
+					$imgAlt = $img.attr('data-alt'),
+					$imgExt = $imgAlt.split('.');
+					
+			if($imgExt[1] === 'gif') {
+				$img.attr('src', $img.data('alt')).attr('data-alt', $imgSrc);
+			} else {
+				$img.attr('src', $imgAlt).attr('data-alt', $img.data('alt'));
+			}
+
+			// Add play class to help with the styling.
+			$this.toggleClass('play');
+
+		});
+
+	})(jQuery);
+
+
 });
+
+
 
 
