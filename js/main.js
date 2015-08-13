92547c8886460327bdde1258c926dfcbd03c812c
Vandana Pai <vandana@vertigo.com>
2015-08-12 17:08:09 -0700

    project pages now load through ajax within the boundaries of the main content container

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-    $("#project-tile-boost").click(function() {
-    	$("#main-content-container").load("work/901cocktails.html");
-    });
+	$('#project-tile-boost').click(function(e){
+		$('#project-grid-container').fadeOut();
+	    e.preventDefault();
+	    $.ajax({
+	        type: "GET",
+	        url: "work/boost.html",
+	        data: { },
+	        success: function(data){
+	            $('#main-content-container').html(data);
+	        }
+	    });
+	});
 
 });
+
