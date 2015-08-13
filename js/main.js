bd9b7255a7eafd02e5ec008f350df1bca5d73354
Vandana Pai <vandana@vertigo.com>
2015-08-13 10:51:51 -0700

    vp logo color now changes to project color when on that page

--- js/main.js
+++ js/main.js
@@ -7,11 +7,15 @@ $(document).ready(function() {
 
 		//get the link location that was clicked
 		pageurl = $(this).attr('href');
+		
+			// change color of VP logo		
+			pageColor = $(this).attr('data-color');
+			$('#background-fill, #p-fill').attr('class', pageColor);
 
 		//to get the ajax content and display in div with id 'content'
 		$.ajax({url:pageurl+'?rel=get',success: function(data){
 			$('#main-content-container').html(data);
-			$("html, body").animate({ scrollTop: 0 }, 200);
+			$('html, body').animate({ scrollTop: 0 }, 200);
 		}});
 
 		//to change the browser URL to the given link location
