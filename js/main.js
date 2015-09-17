8975351103f4860d11271ce0a310c587920bd01f
Vandana Pai <vandana@vertigo.com>
2015-09-17 14:51:41 -0700

    website is now just static pages, content is no longer loaded in through ajax

--- js/main.js
+++ js/main.js
@@ -2,40 +2,38 @@ $(document).ready(function() {
 
 	$(function(){
 		$("a[rel='get']").click(function(e){
-		e.preventDefault();
-		$('#project-grid-container').fadeOut();
-
+		// e.preventDefault();
 		//get the link location that was clicked
 		pageurl = $(this).attr('href');
-		
+
 			// change color of VP logo		
 			pageColor = $(this).attr('data-color');
 			$('#background-fill, #p-fill').attr('class', pageColor);
 
 
-		//to get the ajax content and display in div with id 'content'
-		$.ajax({url:pageurl+'?rel=get',success: function(data){
-			var result = $(data).filter('#ajax-loaded-content').html(); //test
-			console.log(result); 
-			$('#main-content-container').html(data);
-			$('html, body').animate({ scrollTop: 0 }, 200); // scroll to top 
-		}});
+		// //to get the ajax content and display in div with id 'content'
+		// $.ajax({url:pageurl+'?rel=get',success: function(data){
+		// 	var result = $(data).filter('#ajax-loaded-content').html(); //test
+		// 	console.log(result); 
+		// 	$('#main-content-container').html(data);
+		// 	$('html, body').animate({ scrollTop: 0 }, 200); // scroll to top 
+		// }});
 
-		//to change the browser URL to the given link location
-		if(pageurl!=window.location){
-		window.history.pushState({path:pageurl},'',pageurl);
-		}
-		//stop refreshing to the page given in
-		return false;
+		// //to change the browser URL to the given link location
+		// if(pageurl!=window.location){
+		// window.history.pushState({path:pageurl},'',pageurl);
+		// }
+		// //stop refreshing to the page given in
+		// return false;
 		});
 
 		/* the below code is to override back button to get the ajax content without page reload*/
-		$(window).bind('popstate', function() {
-			$.ajax({url:location.pathname+'?rel=get',success: function(data){
-				var result = $(data).filter('#ajax-loaded-content').html();
-				$('#main-content-container').html(data);
-			}});
-		});
+		// $(window).bind('popstate', function() {
+		// 	$.ajax({url:location.pathname+'?rel=get',success: function(data){
+		// 		var result = $(data).filter('#ajax-loaded-content').html();
+		// 		$('#main-content-container').html(data);
+		// 	}});
+		// });
 	});
 
 
